136a86f8319dc56a69f35539c294d93e31f2bbbb
Elcins <elcins.gasanovs@gmail.com>
2022-07-27 23:51:18 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,113 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-
-
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    // Define the entry points of our application (can be multiple for different sections of a website)
+    entry: {
+        main: "./src/index.js",
+    },
+
+    // Define the destination directory and filenames of compiled resources
     output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js',
+        filename: "[name].js",
+        path: path.resolve(process.cwd(), "./public"),
     },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, './build'),
-        },
-        compress: true,
-        port: 9000,
-        open: true,
-        hot: true
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.html"
-    }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css"
-        }),
-        new CopyPlugin({
-            patterns: [
-                {from: 'assets/', to: 'assets'},
-            ]
-        }),
-    ],
+
+    // Define development options
+    devtool: "source-map",
+
+    // Define loaders
     module: {
         rules: [
+            // Use babel for JS files
             {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env"
+                        ]
+                    }
+                }
             },
+            // CSS, PostCSS, and Sass
             {
-                test: /\.scss$/,
+                test: /\.(scss|css)$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: true,
+                            url: false,
+                        }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    "autoprefixer",
+                                ]
+                            }
+                        }
+                    },
                     "sass-loader"
-                ]
+                ],
             },
-        ]
+            // File loader for images
+            {
+                test: /\.(jpg|jpeg|png|git|svg)$/i,
+                type: "asset/resource",
+            }
+        ],
     },
-    resolve: {
-        extensions: ['*', '.js']
+
+    // Define used plugins
+    plugins: [
+
+        // Extracts CSS into separate files
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+            chunkFilename: "[id].css"
+        }),
+
+        // Copy images to the public folder
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src/assets/images",
+                    to: "images",
+                }
+            ]
+        }),
+
+        // Inject styles and scripts into the HTML
+        new HtmlWebpackPlugin({
+            template: path.resolve(process.cwd(), "./src/index.html")
+        })
+    ],
+
+    // Configure the "webpack-dev-server" plugin
+    devServer: {
+        static: {
+            directory: path.resolve(process.cwd(), "public")
+        },
+        watchFiles: [
+            path.resolve(process.cwd(), "./src/index.html")
+        ],
+        compress: true,
+        port: process.env.PORT || 9090,
+        hot: true,
     },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin()
-        ]
+
+    // Performance configuration
+    performance: {
+        hints: false
     }
-}
+};
